f0af9ee714a547fe4fcd33259bfb0bad6edf2aed
Jose <josesalazar08@gmail.com>
2022-10-10 21:38:40 -0400

    created java file

---
+++ javascript.js
@@ -0,0 +1,64 @@
+/*console.log("Hello, World!")
+
+let message = 'Hello Mofos!';
+
+alert(message);
+
+let user = 'John';
+let age = '25';
+
+console.log(23+97);
+
+console.log(2+2+2+2+2+2);
+console.log((4 + 6 + 9) / 77);
+
+let a = 10
+console.log(a);
+
+console.log(9 * a);
+
+let b = 7 * a;
+console.log(b);
+
+const max = 57;
+let actual = max - 13;
+let percentage = actual / max;
+
+console.log(percentage);
+
+const string = "The revolution will not be televised.";
+/*console.log(string);
+
+const sgl = 'Single Quotes';
+const dbl = "double quotes";
+console.log(sgl);
+console.log(dbl);
+
+const bigMouth = "I\'ve got no right to take my place";
+console.log(bigMouth);
+
+
+const name = 'Chris';
+const greeting = `Hello, ${name}`;
+console.log(greeting);
+
+const one = 'Hello, ';
+const two = 'how are you?';
+const joined = `${one}${two}`;
+console.log(joined);
+*/
+/*const button = document.querySelector("button");
+
+function greet(){
+    const name = prompt("What is your name?");
+    alert(`Hello ${name}, nice to see you!`);
+
+}
+
+button.addEventListener("click", greet);
+*/
+
+const name = "Front";
+const number = 242;
+console.log(`${name} ${number}`);
+
