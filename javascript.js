552a554a7c3cb24bbaf8c1ed799fe94370e4c7f1
zvlvs <josesalazar08@gmail.com>
2022-11-10 14:06:42 -0500

    updated java training

--- javascript.js
+++ javascript.js
@@ -436,3 +436,166 @@ function golfScore(par, strokes) {
 }
 
 console.log(golfScore(5, 9));
+
+//case switch
+
+function caseInSwitch(val) {
+    var answer = "";
+    switch(val) {
+        case 1: 
+            answer = "alpha";
+            break;
+        case 2:
+            answer = "beta";
+            break;
+        case 3: 
+            answer = "gamma";
+            break;
+        case 4:
+            answer = "delta";
+            break;
+    }
+    return answer;
+}
+
+console.log(caseInSwitch(3));
+
+//default option in switch statements
+
+function switchOfStuff(val) {
+    var answer = "";
+    switch(val) {
+        case "a":
+            answer = "apple";
+            break;
+        case "b":
+            answer = "bird";
+            break; 
+        case "c":
+            answer = "cat";
+            break;
+        case "d":
+            answer = "dog";
+            break;   
+        default: //default operates as an else function
+            answer = "stuff";
+            break;
+    }
+    return answer;
+}
+
+console.log(switchOfStuff(4));
+
+//mutliple identical options in switch statements
+
+function sequentialSizes(val) {
+    var answer = "";
+    switch(val) {
+        case 1:
+        case 2:
+        case 3:
+            answer = "Low";
+            break;
+        case 4:
+        case 5:
+        case 6:
+            answer = "mid";
+            break;
+        case 7:
+        case 8:
+        case 9: 
+            answer = "high";
+            break;
+    }
+    return answer;
+}
+
+console.log(sequentialSizes(5));
+
+//replacing If Else Chains with switch
+
+function chainToSwitch(val) {
+    var answer = "";
+
+    switch(val) {
+        case "bob": //case replaces else if statements
+            answer = "Marley";
+            break;
+        case 42:
+            answer = "The Answer";
+            break;
+        case 1:
+            answer = "There is no #1";
+            break;
+        case 99:
+            answer = "Missed me by this much";
+            break;
+        case 7:
+            answer = "Ate Nine";
+            break;
+    }
+    return answer;
+}
+
+//returning boolean values from functions
+
+function isLess(val) {
+    if (a < b) { //this is one way of returning boolean functions
+        return true;
+    } else {
+        return false;
+    }
+}
+
+console.log(isLess(10,15));
+
+//a more compact way of returning boolean values would be the following
+function isLessAlso(val){
+    return a < b;
+}
+
+console.log(isLessAlso(10,15));
+
+//returning early pattern from function
+
+function abTest(val) {
+    if (a < 0 || b < 0) {
+        return undefined;
+    }
+}
+
+console.log(abTest(-2,2));
+
+
+//counting cards
+var count = 0;
+
+function cc(card) {
+    switch(card) {
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+        case 6:
+            count++;
+            break;
+        //we will omit 7,8,9 because the program will do nothing with these numbers
+        case 10:
+        case "J":
+        case "Q":
+        case "K":
+        case "A":
+            count--;
+            break;
+    }
+
+    var holdbet = 'Hold'
+    if (count > 0) {
+        holdbet = 'Bet'
+    }
+
+    return count + " " + holdbet;
+}
+
+cc(2); cc("K"); cc(10); cc("K"); cc("A");
+console.log(cc(4));
